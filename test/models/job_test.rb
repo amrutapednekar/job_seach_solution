59fe08946ce11d819b89af2137ef40d245c44b82
amrutapednekar <amruta.jadhav1@gmail.com>
2022-02-02 13:04:15 +0100

    Unit test cases for Jobs

--- test/models/job_test.rb
+++ test/models/job_test.rb
@@ -4,4 +4,20 @@ class JobTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should not create a job without title" do
+    job = Job.new(title: "")
+    assert_not job.save, "Created a job without title"
+  end
+
+  test "should not create a job without spoken language" do
+    job = Job.new(title:"Senior Developer",languages:[])
+    assert_not job.save, "Created a job without spoken language"
+  end
+
+  test "Current user must exist" do
+    job = Job.new(title: "Senior Developer1",languages: ["English"] ,shift_dates:1, salary_per_hour: 40,user_id: nil )
+    assert_not job.save, "User must exist"
+  end
+
 end
