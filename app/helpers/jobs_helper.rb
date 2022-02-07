ba4a0c80704a70f309eb25bf4b5869dcd5e00c8a
amrutapednekar <amruta.jadhav1@gmail.com>
2022-02-08 00:03:37 +0100

    Applying basic CSS

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -3,12 +3,12 @@ module JobsHelper
         return job_user_id == current_user.id
     end
 
-    def is_already_applied(job_id)
-        is_already_applied = UserAppliedJob.where(["user_id = ?, job_id = ? and status = ?", current_user.id, job_id, true ])
-        unless is_already_applied.nil?
-             return true
+    def is_already_applied?(job_id)
+        is_already_applied = UserAppliedJob.where(["user_id = ? and job_id = ?", current_user.id, job_id])
+        if is_already_applied.blank?
+             return false
         else
-            return false
+            return true
         end
     end
 
